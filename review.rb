2c7eca4df3d03dcd56961cc61561ba626c056567
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-05-14 10:26:34 -0600

    message about vim

---
+++ review.rb
@@ -0,0 +1,11 @@
+# VIm NOTES to ype we need to get in insert mode by type the char 'l'
+#to get in prompt mode we use esc key
+#to get in prompt mode we type comman + :
+#q with out saving 
+#w save
+#wq save and quit 
+
+
+x =1 
+
+puts x
